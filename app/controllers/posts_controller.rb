0f8fdd558c2a26543f755a3a6c7575cdb5063954
Pablolizardi <pabloagra3018@gmail.com>
2024-03-01 15:45:08 -0400

    server, db, html up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
 class PostsController < ApplicationController
     skip_before_action :verify_authenticity_token
+    def index
+        @new_post = Post.new
+        @all_posts = Post.order(created_at: :desc).all
+      end
 end
